ba1b9059fbefe7bb1d812270154b7cd7c322c572
ChristopherZev <christopher_zev@icloud.com>
2017-08-01 18:19:50 -0400

    imported jquery for json api call, made Todos and Todo Item with Proejct and Project Item code to display api data, add PropTypes to new files and made sure components are passing up to App.js correctly

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import uuid from 'uuid';
+import $ from 'jquery';
 import Projects from './Components/Projects';
 import AddProject from './Components/AddProject';
+import Todos from './Components/Todos';
 import './App.css';
 
 
@@ -9,15 +11,31 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      projects:[]
+      projects:[],
+      todos:[]
     }
   }
 
-//data for keys should not be in constrctor
-//should be in a WillMount to run at each render like examples below 
-//LifeCycle Method:
-  componentWillMount(){ 
-    this.setState({projects: [
+getTodos(){
+  $.ajax({
+    url: 'https://jsonplaceholder.typicode.com/todos',
+    dataType: 'json',
+    cache: false,
+    success: function(data){
+      this.setState({todos: data}, function(){
+        console.log(this.state);
+      });
+
+    }.bind(this),
+    error: function(xhr, status, err){
+      console.log(err);
+    }
+  });
+
+}
+
+getProjects(){
+  this.setState({projects: [
       {
           id:uuid.v4(),
           title: 'Business Website',
@@ -35,6 +53,19 @@ class App extends Component {
         }
 
       ]});
+
+}
+//data for keys should not be in constrctor
+//should be in a WillMount to run at each render like examples below 
+//LifeCycle Methods:
+  componentWillMount(){ 
+    this.getProjects();
+    this.getTodos();
+
+  }
+
+  componentDidMount(){
+    this.getTodos();
 
   }
 
@@ -60,6 +91,8 @@ class App extends Component {
       <div className="App">
         <AddProject addProject={this.handleAddProject.bind(this)} />
         <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)} />
+        <hr/>
+        <Todos todos={this.state.todos} />
       </div>
     );
   }
