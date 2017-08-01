ff5da91b4a3cc753fa47a3e3f0e8a05e2e8cee7c
ChristopherZev <christopher_zev@icloud.com>
2017-08-01 16:21:31 -0400

    App.js - added handleAddPorject func that push data up from projects component and feeds it to the main App component, AddProject.js - modified handleSubmit to give alert if Title is submitted empty, set the state of newProject with title and category keys from constructor, added call back func to pass data up to App Component

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -3,23 +3,40 @@ import React, { Component } from 'react';
 
 
 class AddProject extends Component {
+	constructor(){
+		super();
+		this.state = {
+			newProject:{}
+		}
+	}
 
 	static defaultProps = {
 		categories: ['Web Design', 'Web Development', 'Mobile Development']
 	}
 
 	handleSubmit(e){
-		console.log('Submiited');
+		if(this.refs.title.value === ''){
+			alert("Please Enter a Title!");
+		} else{
+			this.setState({newProject: {
+				title: this.refs.title.value,
+				category: this.refs.category.value
+
+			}}, function(){
+				//console.log(this.state)
+				this.props.addProject(this.state.newProject);
+			});
+		}
 		e.preventDefault();
 	}
 	render() {
 		let categoryOptions = this.props.categories.map(category =>{
-			return <option key={category} value="category">{category}</option>
+			return <option key={category} value={category}>{category}</option>
 		});
     	return (
 	      <div>
 	        <h3>Add Project</h3>
-	        <form onSubmit={this.handleSubmit}>
+	        <form onSubmit={this.handleSubmit.bind(this)}>
 	        	<div>
 	        		<label htmlFor="">Title</label><br/>
 	        		<input type="text" ref="title"/>
