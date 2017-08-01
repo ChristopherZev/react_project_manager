9b500b73088dd0853938fa749af3cb0a13e0d3c6
ChristopherZev <christopher_zev@icloud.com>
2017-08-01 11:42:57 -0400

    Added Lifecyle method componentWillMountto handle data instead of having data in contrusctor, added AddProject Component with title form, category drop down and submit button, Drop down uses static defaultProps to popluate.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 
 
@@ -7,8 +8,16 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      projects:[
-        {
+      projects:[]
+    }
+  }
+
+//data for keys should not be in constrctor
+//should be in a WillMount to run at each render like examples below 
+//LifeCycle Method:
+  componentWillMount(){ 
+    this.setState({projects: [
+      {
           title: 'Business Website',
           category: 'Web Design'
         },
@@ -20,16 +29,17 @@ class App extends Component {
           title: 'Ecommerce Shopping cart',
           category: 'Web Development'
         }
-      ]
-    }
+
+      ]});
+
   }
 
 
   render() {
-    
+
     return (
       <div className="App">
-        My App
+        <AddProject />
         <Projects projects={this.state.projects}/>
       </div>
     );
